9eb9472005d385e59739da663996412d695ea7fa
AntoineVRG <antoine.vergez@epitech.eu>
2024-04-11 22:25:47 +0200

    [UPDATE] LoginPage

--- enefte/src/pages/LoginPage.js
+++ enefte/src/pages/LoginPage.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Typography, TextField, Button } from '@mui/material';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const LoginPage = () => {
+  const [walletID, setWalletID] = useState('');
+  const [newWalletID, setNewWalletID] = useState('');
+
+  // Simuler la création d'un wallet
+  const handleCreateWallet = () => {
+    // Simuler un ID de wallet généré
+    const generatedWalletID = 'xrp1234567890';
+    setNewWalletID(generatedWalletID);
+  };
+
+  // Fonction pour copier l'ID du Wallet dans le presse-papiers
+  const handleCopyToClipboard = () => {
+    if (newWalletID) {
+      navigator.clipboard.writeText(newWalletID).then(() => {
+        alert('Wallet ID copied to clipboard!'); // Notification de la copie réussie
+      }, (err) => {
+        console.error('Could not copy text: ', err); // Gérer les erreurs de copie
+      });
+    }
+  };
+
   return (
     <Box sx={{
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-      height: '100%',
+      height: '100vh',
       width: '100%'
     }}>
       <Box sx={{
@@ -25,29 +47,55 @@ const LoginPage = () => {
           Login or Register
         </Typography>
         <Typography sx={{ color: 'white', mb: 2 }}>
-          Enter your email address and password manually
-        </Typography>
-        <Typography sx={{ color: 'white', mb: 1, fontWeight: 'bold' }}>
-          Email Address
+          Enter your Wallet ID
         </Typography>
         <TextField
           variant="filled"
           fullWidth
-          placeholder="Enter your email address"
+          value={walletID}
+          onChange={(e) => setWalletID(e.target.value)}
+          placeholder="Enter your Wallet ID"
           sx={{ mb: 2, input: { color: 'white' }, backgroundColor: 'rgba(255,255,255,0.1)' }}
         />
-        <Typography sx={{ color: 'white', mb: 1, fontWeight: 'bold' }}>
-          Password
-        </Typography>
-        <TextField
-          variant="filled"
-          fullWidth
-          placeholder="Enter your password"
-          sx={{ mb: 2, input: { color: 'white' }, backgroundColor: 'rgba(255,255,255,0.1)' }}
-        />
-        <Button variant="contained" sx={{ mt: 2, bgcolor: 'white', color: '#1E292E' }}>
-          Continue
+        <Button variant="contained" sx={{ 
+            mt: 2, 
+            bgcolor: 
+            'white', 
+            color: '#1E292E' , 
+            ':hover': {
+                backgroundColor: 'grey.300'
+            }}}>
+            Connect
         </Button>
+        <Button onClick={handleCreateWallet} variant="contained" sx={{
+            mt: 2, 
+            bgcolor: 'white', 
+            color: '#1E292E', 
+            marginTop: '10px', 
+            ':hover': {
+                backgroundColor: 'grey.300'
+            }}}>
+            Create Wallet
+        </Button>
+        {newWalletID && (
+          <>
+            <Box sx={{ display: 'flex', alignItems: 'center', color: 'white', mt: 2 }}>
+                <Typography sx={{ mr: 1 }}>
+                New Wallet ID: {newWalletID}
+                </Typography>
+                <Button onClick={handleCopyToClipboard} sx={{ 
+                    minWidth: '10px', 
+                    padding: '4px', 
+                    bgcolor: 'white', 
+                    color: '#1E292E', 
+                    ':hover': {
+                        backgroundColor: 'grey.300'
+                }}}>
+                    <ContentCopyIcon fontSize="small" />
+                </Button>
+            </Box>
+          </>
+        )}
       </Box>
     </Box>
   );
