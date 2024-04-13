c2a8858ee98c41c07aebf387916caa44383555fb
AntoineVRG <antoine.vergez@epitech.eu>
2024-04-13 10:54:42 +0200

    [UPDATE]  Home

--- enefte/src/pages/MyTokens.js
+++ enefte/src/pages/MyTokens.js
@@ -1,68 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import NFTCard from '../components/nftCards';
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, CircularProgress, Typography } from '@mui/material';
 
 const MyTokens = () => {
     const [nfts, setNfts] = useState([]);
-
-    function concatNfts(nftsData) {
-        let concatenatedNfts = [];
-    
-        nftsData.forEach(walletData => {
-            walletData.nfts.forEach(nft => {
-                let concatenatedNft = {
-                    nftId: nft.NFT_ID,
-                    title: nft.metadata.title,
-                    description: nft.metadata.description,
-                    url: nft.metadata.url
-                };
-                concatenatedNfts.push(concatenatedNft);
-            });
-        });
-    
-        return concatenatedNfts;
-    }
-
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
 
     useEffect(() => {
-
-        const fetchNFTs = async () => {
-            const url = 'http://localhost:3000/nft/findAll';
-            const accessToken = sessionStorage.getItem('accessToken');
-            const formData = new URLSearchParams();
-            formData.append('token', accessToken);
-            try {
-                const response = await fetch(url, {
-                    method: 'GET',
-                    headers: {
-                        'Authorization': 'Bearer ' + accessToken,
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                });
-                const data = await response.json();
-                console.log(data)
-                if (response.ok) {
-                    const tmp = concatNfts(data);
-                    setNfts(tmp)
-                } else {
-                    throw new Error('Fail sending request');
-                }
-            } catch (error) {
-                console.error('Login error:', error);
+        setIsLoading(true);
+        const url = 'http://localhost:3000/nft/findAll';
+        const accessToken = sessionStorage.getItem('accessToken');
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.length > 0) {
+                console.log("Data received:", data);
+                setNfts(data);
+            } else {
+                setError("No tokens available at the moment.");
             }
-        }
-        fetchNFTs();
+            setIsLoading(false);
+        })
+        .catch(error => {
+            console.error("Error fetching tokens:", error);
+            setError(error.message);
+            setIsLoading(false);
+        });
     }, []);
 
     return (
-        <Box sx={{ width: '100%', marginLeft: 'auto' }}>
-            <Grid container spacing={5} margin={'auto'} width={'90%'}>
-                {nfts.map(nft => (
-                    <Grid item key={nft.nftId} xs={12} sm={6} md={3}>
-                        <NFTCard nft={nft} />
-                    </Grid>
-                ))}
-            </Grid>
+        <Box sx={{ width: '100%', padding: 3 }}>
+            {isLoading ? (
+                <CircularProgress />
+            ) : error ? (
+                <Typography color="error">{error}</Typography>
+            ) : (
+                <Grid container spacing={2}>
+                    {nfts.map(nft => (
+                        <Grid item key={nft.NFT_ID} xs={12} sm={6} md={3}>
+                            <NFTCard nft={nft.metadata} />
+                        </Grid>
+                    ))}
+                </Grid>
+            )}
         </Box>
     );
 };
